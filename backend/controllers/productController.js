57a9f35c693ee28faa0b3a2a84a90586d163acc4
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2024-11-20 18:47:26 +0800

    first commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+const Product = require('../models/product')
+const mongoose = require('mongoose')
+
+
+//Create Product
+const createProduct = async(req, res) =>{
+    const {name, price, description} = req.body
+
+    try{
+        const prodcut = await Product.create({name, price, description})
+        res.status(200).json(product)
+    }catch(error){
+        res.status(500).json({error: err})
+    }
+}
+
+//Get all Products
+const getProducts = async(req, res) =>{
+    const products = await Product.find({})
+    res.status(200).json(products)
+}
+
+module.exports = {
+    createProduct,
+    getProducts
+}
---
+++ backend/server.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const productRoutes = require('./routes/productRoutes')
+const app = express()
+
+//cors enabled endpoints
+let corsOptions = {
+    origin: []
+}
+
+//middleware
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+mongoose.connect(process.env.DB_URI)
+    .then(() =>{
+        app.listen(process.env.PORT, ()=>{
+            console.log('Connected to database...')
+            console.log(('Listening to port ', process.env.PORT))
+        }).catch(err =>{
+            console.log(err)
+        })
+    })
+
+
+let requestMapper = '/api/v1'
+app.use('/product', productRoutes)
+
+
+//if no request match
+app.use((req, res) =>{
+    res.status(404).json({error: "No such method exist"})
+})
