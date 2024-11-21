33474fa00700204174a329b19a9ebd68badc17d7
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2024-11-21 13:37:34 +0800

    add initial template for product

--- frontend/appchara-admin/src/pages/Product.js
+++ frontend/appchara-admin/src/pages/Product.js
@@ -1,9 +1,25 @@
-import Navbar from "../component/Navbar";
 
 const Product = () => {
     return ( 
         <div className="product">
-            
+            <div className="px-5 py-2">
+                <h1 className="mb-4">Products</h1>
+                <div className="row">
+                    <div className="col col-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <h5 className="card-title">Ampalaya</h5>
+                                        <h6 className="card-subtitle text-secondary">Stock: 100</h6>
+                                    </div>
+                                    <i className="bi bi-three-dots-vertical fs-5"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
