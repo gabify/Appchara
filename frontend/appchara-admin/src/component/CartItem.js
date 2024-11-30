f0eba0b630267ac02842cfb4eb13720635da243b
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2024-11-30 16:13:30 +0800

    add add to order function

--- frontend/appchara-admin/src/component/CartItem.js
+++ frontend/appchara-admin/src/component/CartItem.js
@@ -1,14 +1,43 @@
 import {Card} from 'react-bootstrap'
 
-const CartItem = ({cartItem}) => {
+const CartItem = ({cartItem, deleteItem, addQuantity, reduceQuantity}) => {
+    const handleDelete = () =>{
+        deleteItem(cartItem.id)
+    }
+
+    const handleAddQuantity = () =>{
+        addQuantity(cartItem.id)
+    }
+
+    const handleReduceQuantity = () =>{
+        reduceQuantity(cartItem.id)
+    }
+
     return ( 
         <div className="col col-12 mb-1">
             <Card>
                 <Card.Body>
-                    <div>
-                        <div className='mb-3'>
-                            <Card.Title>{cartItem.product.name}</Card.Title>
-                            <span className='mt-2'>{cartItem.product.price}.00 php</span>
+                    <div className='d-flex justify-content-between'>
+                        <div>
+                            <div className="mb-2">
+                                <Card.Title>{cartItem.product.name}</Card.Title>
+                                <span>{cartItem.price}.00 php</span>
+                            </div>
+                            <div className="d-flex justif-content-evenly">
+                                <i className="bi bi-dash-circle-fill me-2" 
+                                    style={{cursor: "pointer"}}
+                                    onClick={handleReduceQuantity}></i>
+                                <span>{cartItem.quantity}</span>
+                                <i className="bi bi-plus-circle-fill ms-2" 
+                                    style={{cursor: "pointer"}}
+                                    onClick={handleAddQuantity}></i>
+                            </div>
+                        </div>
+                        <div>
+                            <i 
+                                className="bi bi-trash3-fill text-danger" 
+                                style={{cursor: "pointer"}}
+                                onClick={handleDelete}></i>
                         </div>
                     </div>
                 </Card.Body>
--- frontend/appchara-admin/src/pages/Sale.js
+++ frontend/appchara-admin/src/pages/Sale.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react'
+import { Button } from 'react-bootstrap'
 import {useProductContext} from '../hooks/useProductContext'
 import POSProductCard from '../component/POSProductCard'
 import CartItem from '../component/CartItem'
@@ -7,11 +8,90 @@ const Sale = () => {
     const {products, dispatch} = useProductContext()
     const [cart, setCart] = useState([])
     const [productId, setProductId] = useState(0)
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [error, setError] = useState(null)
 
     const addToCart = (product) =>{
         let id = productId
-        setCart([...cart, {id: id++, product, quantity:0}])
-        setProductId(id)
+        const cartItem = {id, product, quantity:1, price: product.price}
+        setProductId(id + 1)
+
+        const newCart = [...cart, cartItem]
+        setCart(newCart)
+
+        const newTotal = newCart.reduce((sum, item) => sum + item.price, 0);
+        setTotalPrice(newTotal);
+    }
+
+    const removeToCart = (id) =>{
+        setCart(cart.filter((cartItem) => cartItem.id !== id))
+        let total = totalPrice
+        cart.map(cartItem =>{
+            if(cartItem.id === id){
+                total -= cartItem.price
+            }
+
+            return cartItem
+        })
+
+        setTotalPrice(total)
+
+    }
+
+    const addQuantity = (id) =>{
+        const newCart = cart.map(cartItem =>{
+            if(cartItem.id === id){
+                cartItem.quantity += 1
+                cartItem.price = cartItem.product.price * cartItem.quantity
+            }
+
+            return cartItem
+        })
+
+        setCart(newCart)
+
+        const newTotal = newCart.reduce((sum, item) => sum + item.price, 0);
+        setTotalPrice(newTotal);
+        
+    }
+
+    const reduceQuantity = (id) =>{
+        const newCart = cart.map(cartItem =>{
+            if(cartItem.id === id && cartItem.quantity > 1){
+                cartItem.quantity -= 1
+                cartItem.price = cartItem.product.price * cartItem.quantity
+            }
+
+            return cartItem
+        })
+
+        setCart(newCart)
+
+        const newTotal = newCart.reduce((sum, item) => sum + item.price, 0);
+        setTotalPrice(newTotal);
+    }
+
+    const handleCheckOut = async() =>{
+        const sales = cart.map(({id, product, price, ...rest}) => ({
+            product_id:product._id,
+            sale_per_item: price,
+            ...rest
+        }))
+        const sale = {sales, total_sale: totalPrice}
+        console.log(sale)
+        const response = await fetch('http://127.0.0.1:5000/api/v1/sale/new', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(sale)
+        })
+        const result = await response.json()
+
+        if(!response.ok){
+            setError(result.error)
+            console.log(error)
+        }
     }
 
     useEffect(() =>{
@@ -44,10 +124,21 @@ const Sale = () => {
                             
                         </div>
                     </div>
-                    <div className="col col-4" style={{overflowY: "auto", maxHeight: "500px"}}>
-                        {cart.map((cartItem) =>(
-                            <CartItem key={cartItem.id} cartItem={cartItem} />
-                        ))}
+                    <div className="col col-4">
+                        <div style={{overflowY: "auto", height: "500px"}}>
+                            {cart.map((cartItem) =>(
+                                <CartItem 
+                                    key={cartItem.id} 
+                                    cartItem={cartItem} 
+                                    deleteItem={removeToCart}
+                                    addQuantity = {addQuantity}
+                                    reduceQuantity={reduceQuantity} />
+                            ))}
+                        </div>
+                        <div>
+                            <p className='mb-0'>Total: {totalPrice}</p>
+                            <Button variant="success" onClick={handleCheckOut}>Checkout</Button>
+                        </div>
                     </div>
                 </div>
             </div>
