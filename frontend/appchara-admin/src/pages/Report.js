44f728b2ca4bf22cd9f1162893c37474d92acd1f
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2024-12-18 13:23:59 +0800

    add iniital design for reports

--- frontend/appchara-admin/src/pages/Report.js
+++ frontend/appchara-admin/src/pages/Report.js
@@ -1,13 +1,62 @@
-import SideNav from "../component/SideNav";
+import { useState, useEffect } from "react";
+import {Table} from 'react-bootstrap'
 
 const Report = () => {
+    const [sales, setSales] = useState(null)
+    const [error, setError] = useState(null)
+    const [totalSales, setTotalSales] = useState(0)
+
+    useEffect(() =>{
+        const fetchSales = async() =>{
+            const response = await fetch('http://127.0.0.1:5000/api/v1/sale/')
+            const result = await response.json()
+
+            if(response.ok){
+                setSales(result)
+                const total = result.reduce((sum, sale) => sum + sale.total_sale, 0);
+                setTotalSales(total)
+
+            }else{
+                setError(result.error)
+                console.log(error)
+            }
+        }
+
+        fetchSales()
+    }, [error])
+
     return ( 
-        <div className="row g-0">
-            <div className="col col-md-3 vh-100">
-                <SideNav/>
-            </div>
-            <div className="col col-md-auto">
-                <h1>Content of Report</h1>
+        <div className="report">
+            <div className="px-5 py-2">
+                <Table bordered hover className="text-center">
+                    <thead>
+                        <tr>
+                            <th>Transaction Id</th>
+                            <th>Date</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {sales && sales.map((sale) =>{
+                            const formatedDate = new Date (sale.createdAt).toLocaleDateString('en-US', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: "numeric"
+                            })
+                            return (
+                                <tr>
+                                    <td>{sale._id}</td>
+                                    <td>{formatedDate}</td>
+                                    <td>₱ {sale.total_sale}.00</td>
+                                </tr>
+                            )
+                        })}
+                        <tr>
+                            <td colSpan={2} className="fw-bold text-end">Total Sales:</td>
+                            <td className="fw-bold">₱ {totalSales}.00</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         </div>
      );
