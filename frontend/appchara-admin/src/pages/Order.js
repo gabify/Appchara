9349a4653b049ac67be253cbc4a0a10085c61c56
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2024-12-20 14:53:29 +0800

    add style on order page

--- frontend/appchara-admin/src/pages/Order.js
+++ frontend/appchara-admin/src/pages/Order.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
+import { Tabs, Tab, Badge } from 'react-bootstrap'
 import OrderCard from '../component/OrderCard'
 
 const Order = () => {
     const [orders, setOrders] = useState([])
+    const [key, setKey] = useState('all')
 
     useEffect(() =>{
         const fetchOrder = async() =>{
@@ -17,16 +19,115 @@ const Order = () => {
         fetchOrder()
     }, [])
 
+    function TabOrders ({orders, status}){       
+        if(!Array.isArray(orders) || orders.length === 0){
+            return <div>No orders at the moment!</div> 
+        }
+
+        let filteredOrders;
+
+        switch(status){
+            case 'pending':
+                filteredOrders = orders.filter((order) => order.status === 'pending')
+                break;
+            case 'complete':
+                filteredOrders = orders.filter((order) => order.status === 'complete')
+                break;
+            case 'cancelled':
+                filteredOrders = orders.filter((order) => order.status === 'cancelled')
+                break;
+            default:
+                filteredOrders = orders
+        }
+        if(filteredOrders.length === 0){
+            return(
+                <div>No {status} orders at the moment!</div>
+            )   
+        }
+
+        return(
+            <div className="row">
+                {filteredOrders.map((order) =>(
+                    <OrderCard order={order} key={order._id}/>
+                ))}
+            </div>
+        )
+    }
+
+    function OrderBadge({orders, status}){
+        if(!Array.isArray(orders) || orders.length === 0){
+            <Badge pill bg="danger" className="ms-2">0</Badge>
+        }
+
+        let filteredOrders;
+
+        switch(status){
+            case 'pending':
+                filteredOrders = orders.filter((order) => order.status === 'pending')
+                break;
+            case 'complete':
+                filteredOrders = orders.filter((order) => order.status === 'complete')
+                break;
+            case 'cancelled':
+                filteredOrders = orders.filter((order) => order.status === 'cancelled')
+                break;
+            default:
+                filteredOrders = orders
+        }
+        if(filteredOrders.length === 0){
+            return <Badge pill bg="danger" className="ms-2">0</Badge>   
+        }
+
+        return(
+            <Badge pill bg="danger" className="ms-2">
+                {filteredOrders.length}
+            </Badge>
+        )
+    }
+
 
     return ( 
         <div className="orders">
             <div className="px-5 py-2">
-                <div className="row">
-                    {orders && orders.map((order) =>(
-                        <OrderCard order={order} key={order._id}/>
-                    ))}
-                    
-                </div>
+                <Tabs
+                    id="order-tab"
+                    activeKey={key}
+                    onSelect={(k) => setKey(k)}
+                    className="mb-3"
+                    >
+                    <Tab eventKey="all" title={
+                        <>
+                            All Orders 
+                            <OrderBadge orders={orders}/>
+                        </>
+                    }>
+                        <TabOrders orders={orders}/>
+                    </Tab>
+                    <Tab eventKey="pending" title={
+                        <>
+                            Pending Orders 
+                            <OrderBadge orders={orders} status={"pending"}/>
+                        </>
+                    }>
+                        <TabOrders orders={orders} status={'pending'}/>
+                    </Tab>
+                    <Tab eventKey="complete" title={
+                        <>
+                            Completed Orders 
+                            <OrderBadge orders={orders} status={"complete"}/>
+                        </>
+                    }>
+                        <TabOrders orders={orders} status={'complete'}/>
+                    </Tab>
+                    <Tab eventKey="cancelled" title={
+                        <>
+                            Cancelled Orders 
+                            <OrderBadge orders={orders} status={"cancelled"}/>
+                        </>
+                    }>
+                        <TabOrders orders={orders} status={'cancelled'}/>
+                    </Tab>
+                </Tabs>
             </div>
         </div>
      );
